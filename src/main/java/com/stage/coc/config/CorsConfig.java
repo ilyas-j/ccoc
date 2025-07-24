c83530d098d4e7f0c030e26e8be8b7c5661995da
ilyas-j <ilyaskabbouri33@gmail.com>
2025-07-24 18:50:02 +0100

    Fusion des historiques

--- src/main/java/com/stage/coc/config/CorsConfig.java
+++ src/main/java/com/stage/coc/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://votre-frontend.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
